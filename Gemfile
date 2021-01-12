110ad518e96f5bc5b387fc4dcfb5617b11dc6b4c
Kartheek Paturu <kartheek@Kartheeks-Air.Dlink>
2021-01-12 16:50:16 +0530

    Indentation fixed

--- Gemfile
+++ Gemfile
@@ -1,2 +1,2 @@
 source 'https://rubygems.org'
-gem 'rubocop' '1.8.1'
+gem 'rubocop', '1.8.1'
--- main.rb
+++ main.rb
@@ -1,17 +1,18 @@
 def bubble_sort(array)
   array_length = array.size
   return array if array_length <= 1
+
   loop do
-  swapped = false
-  (array_length - 1).times do |i|
+    swapped = false
+    (array_length - 1).times do |i|
         if array[i] > array[i + 1]
           array[i], array[i + 1] = array[i + 1], array[i]
           swapped = true
-        end
+    end
       end
-    break if  !swapped
+    break unless swapped
     end
     array
   end
-  unsorted_array = [4, 3, 78, 2, 0 ,2]
+  unsorted_array = [4, 3, 78, 2, 0, 2]
   p bubble_sort(unsorted_array)
