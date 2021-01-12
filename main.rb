b36f2918627190b5d1fd94d5110b80f169b380a3
Arinpe <dehtolah@gmail.com>
2021-01-12 09:05:53 +0100

    completed bubble_sort

---
+++ main.rb
@@ -0,0 +1,23 @@
+def bubble_sort(array)
+    array_length = array.size
+    return array if array_length <= 1
+  
+    loop do
+  
+      swapped = false
+  
+      (array_length-1).times do |i|
+        if array[i] > array[i+1]
+          array[i], array[i+1] = array[i+1], array[i]
+          swapped = true
+        end
+      end
+  
+      break if not swapped
+    end
+  
+    array
+  end
+  
+  unsorted_array = [4,3,78,2,0,2]
+  p bubble_sort(unsorted_array)
