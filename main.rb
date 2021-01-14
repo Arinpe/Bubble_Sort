1cffb040a03901a6faace81a5d2c7917a5546d73
Arinpe <dehtolah@gmail.com>
2021-01-14 11:01:44 +0100

    worked on bubble_sort_by

--- main.rb
+++ main.rb
@@ -16,3 +16,26 @@ def bubble_sort(array)
 end
 unsorted_array = [4, 3, 78, 2, 0, 2]
 p bubble_sort(unsorted_array)
+
+#bubble-sort-by
+def bubble_sort_by(arr)
+  go_throught_counter = 0
+  loop do
+    arr.each_with_index do |n, i|
+      break if arr[i + 1].nil?
+
+      diff = yield(n, arr[i + 1])
+      if diff.positive?
+        arr[i] = arr[i + 1]
+        arr[i + 1] = n
+      end
+    end
+
+    go_throught_counter += 1
+    break if arr.length - 1 == go_throught_counter
+  end
+  p arr
+end
+bubble_sort_by(%w[hi hello hey]) do |left,right|
+  left.length - right.length
+end
